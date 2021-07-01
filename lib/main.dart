37d2f846251f130b1e12964d76305ec0393e3e67
CiaranJohnson <ciaranmjohnson@aol.com>
2021-07-01 22:37:19 +0100

    Updated UI and buttons to move through a list of books

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-import 'package:judge_a_book_by_its_cover/components/book_info.dart';
+import 'package:judge_a_book_by_its_cover/components/book.dart';
 
 void main() {
   runApp(MyApp());
@@ -40,15 +40,21 @@ class _MyHomePageState extends State<MyHomePage> {
   String bookCoverURL =
       "https://firebasestorage.googleapis.com/v0/b/novela-bbd01.appspot.com/o/Library%2FBookCover%2FHarryPotter%2Fchamber_of_secrets.jpg?alt=media&token=08359d7d-31cd-43e3-ac23-d0f42ce691a7";
 
+  @override
+  void initState() {
+    _makeGetRequest();
+    super.initState();
+  }
+
   void _incrementCounter() {
     setState(() {
       _counter++;
     });
   }
 
-  void makeGetRequest() async {
+  void _makeGetRequest() async {
     http.Response response = await http.get(Uri.parse(
-        'https://www.googleapis.com/books/v1/volumes?q=flowers&orderBy=newest'));
+        'https://www.googleapis.com/books/v1/volumes?q=adventure&orderBy=newest'));
     final responseBody = json.decode(response.body);
     final List<dynamic> books = responseBody['items'];
 
@@ -61,6 +67,10 @@ class _MyHomePageState extends State<MyHomePage> {
       }
     });
 
+    updateUI();
+  }
+
+  void updateUI() {
     setState(() {
       title = bookList[0].title;
       author = bookList[0].authors[0];
@@ -72,28 +82,98 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Text('Judge a Book'),
+        // title: Row(
+        //   children: [IconButton(onPressed:), icon: ],
+        // ),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Image.network(bookCoverURL),
+            Image.network(
+              bookCoverURL,
+              height: (MediaQuery.of(context).size.height * 6) / 10,
+              width: (MediaQuery.of(context).size.width * 9) / 10,
+              fit: BoxFit.fill,
+              loadingBuilder: (context, child, progress) {
+                return progress == null
+                    ? child
+                    : LinearProgressIndicator(
+                        value: (progress.cumulativeBytesLoaded.toDouble() /
+                            progress.expectedTotalBytes!.toDouble()),
+                        backgroundColor: Colors.grey,
+                      );
+              },
+            ),
             Text(
               title,
+              style: Theme.of(context).textTheme.headline4,
             ),
             Text(
               author,
-              style: Theme.of(context).textTheme.headline4,
+              style: Theme.of(context).textTheme.headline6,
+            ),
+            // RawMaterialButton(
+            //   onPressed: () {},
+            //   fillColor: Colors.white,
+            //   child: Icon(
+            //     Icons.bookmark_add,
+            //     color: Colors.green,
+            //     size: MediaQuery.of(context).size.width / 10,
+            //   ),
+            //   padding: EdgeInsets.all(MediaQuery.of(context).size.width / 100),
+            //   shape: CircleBorder(),
+            // ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    bookList.removeAt(0);
+                    if (bookList.length < 3) {
+                      _makeGetRequest();
+                    } else {
+                      updateUI();
+                    }
+                  },
+                  child: Icon(
+                    Icons.close_rounded,
+                    color: Colors.red,
+                    size: MediaQuery.of(context).size.width / 10,
+                  ),
+                  style: ElevatedButton.styleFrom(
+                      shape: CircleBorder(),
+                      padding: EdgeInsets.all(
+                          MediaQuery.of(context).size.width / 100),
+                      primary: Colors.white,
+                      side: BorderSide(
+                        width: 2.0,
+                        color: Colors.red,
+                      )),
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Icon(
+                    Icons.bookmark_add,
+                    color: Colors.green,
+                    size: MediaQuery.of(context).size.width / 10,
+                  ),
+                  style: ElevatedButton.styleFrom(
+                      shape: CircleBorder(),
+                      padding: EdgeInsets.all(
+                          MediaQuery.of(context).size.width / 100),
+                      primary: Colors.white,
+                      side: BorderSide(
+                        width: 2.0,
+                        color: Colors.green,
+                      )),
+                ),
+              ],
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: makeGetRequest,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
---
+++ lib/screens/book_info_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:judge_a_book_by_its_cover/components/book.dart';
+
+class BookInfoScreen extends StatelessWidget {
+  Book book;
+
+  BookInfoScreen({required this.book});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
